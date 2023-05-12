22fb7ad277ed750a223bd5529ead3c92f59f8ae1
Hari Kiran Keerthipati <harikirankeerthipati@gmail.com>
2023-05-12 02:33:07 +0000

    Add all the Netcdf files available in daymet into the data lake. In GCP we will be using cloud storage as our data warehouse.

---
+++ load_nc_files_to_data_lake/load_daymet_dayl_nc_files_to_data_lake.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "downloading the files"
+
+root_url="https://thredds.daac.ornl.gov/"
+path_to_file="thredds/fileServer/ornldaac/2129/"
+
+BUCKET_NAME="data-lake-bucket-climate-modelling"
+directory_name="daylength"
+
+# Extract and store dayl data from 1980 to 2022 into gcloud gcp bucket
+for i in {1980..2022}
+do
+    for locations in "hi" "pr" "na"
+    do
+        nc_file_name="daymet_v4_daily_${locations}_dayl_$i.nc"
+        url_to_download_file=$root_url$path_to_file$nc_file_name
+        
+        wget -O "$nc_file_name" $url_to_download_file
+        
+        gsutil cp "$nc_file_name" "gs://$BUCKET_NAME/$directory_name"
+        
+        rm $nc_file_name
+    done
+done
+
+echo "Done downloading and uploading the netcdf files"
